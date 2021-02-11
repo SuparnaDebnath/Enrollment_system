a467f6063d9ba83a57e9d23ce29dd44272318812
Suparna1662 <suparnadebnath1662@gmail.com>
2021-02-11 10:56:55 +0600

    all

---
+++ dashboard.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col-3 header_height background-1">
+                <a class="logo" href="dashboard.php"><img src="images/IST.png" width="90%" height="90%" alt="IST" /></a>
+
+
+            </div>
+            <div class="col-9 header_height background-1">
+                <h1 style="margin-left: 100px; margin-top: 50px;">Institute of Science and Technology</h1>
+            </div>
+        </div>
+        <!--            <div class="row">
+                        <div class="col-12 menu_height">
+                                            </div>
+                    </div>-->
+        <div class="row">
+            <div class="col-2 content_height background-2">
+                <ul>
+                    <li class="li"><a class="a" href="Dashboard.php">Dashboard</a></li>
+                    <li class="li" ><a class="a" href="Add_Student.php" >Add Student</a></li>
+                    <li class="li" ><a class="a" href="student_info.php">Student Information</a></li>
+                    <li class="li" ><a class="a" href="#">Faculty Member<span class="sub_arrow"></span> </a>
+                        <ul>
+                            <li class="li1"><a class="a1" href="add_member.php">Add Member</a></li>
+                            <li class="li1"><a class="a1" href="faculty_members.php">Faculty Members</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+            </div>
+            <div class="col-9 content_height ">
+                <a class="logo" href="dashboard.php"><img src="images/Project.png" width="100%" height="90%" alt="IST" style="margin: 80px;" /></a> 
+            </div>
+            <div class="col-1 content_height background-1" ></div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 footer_height background-1"></div>
+        </div>
+    </div>
+
+</body>
+</html>
